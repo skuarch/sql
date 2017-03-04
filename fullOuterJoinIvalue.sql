adc7f866fd20f007962eab9650f058c4ebf0e395
skuarch <skuarch@yahoo.com.mx>
2017-03-04 10:18:40 -0600

    full outer join postgresql

---
+++ fullOuterJoinIvalue.sql
@@ -0,0 +1,7 @@
+﻿select u.user_id, u.name
+  from user_policy_details as upd
+  full join users as u
+    on u.user_id = upd.user_id
+where user_policy_details_id is null
+  -- left join policy as p
+  -- p.policy_id = upd.policy_id
